c224b8df03886acbae0f7b6fa7681942f50a75aa
Wandonium <hillarywando@gmail.com>
2023-02-13 09:15:58 +0300

    Still fixing issues with loading the App.js app in browser.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import { Box } from '@mui/material';
 import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from './components';
 
 const App = () => {
+    console.log('testing app');
     <BrowserRouter>
         <Box sx={{ backgroundColor: '#000'}}>
+            <div>testing 123...</div>
             <Navbar />
             <Routes>
                 <Route path="/" exact element={<Feed />} />
--- src/index.js
+++ src/index.js
@@ -5,5 +5,7 @@ import App from './App';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log("root: ", root);
+console.log("what is going on?");
 
 root.render(<App />);
