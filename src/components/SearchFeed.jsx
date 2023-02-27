fc3c030e80b7164e70a344e08286ff97b5006e2e
Wandonium <hillarywando@gmail.com>
2023-02-27 17:32:34 +0300

    Completed the SearchBar and SearchFeed components. Search functionality now works as expected.

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -2,40 +2,28 @@ import { useState, useEffect } from 'react';
 import { Box, Typography } from '@mui/material';
 import { Videos } from './';
 import { fetchFromAPI } from '../utils/fetchFromAPI';
+import { useParams } from 'react-router-dom';
 
-const Feed = () => {
+const SearchFeed = () => {
 
-  const [selectedCategory, setSelectedCategory] = useState('New');
   const [videos, setVideos] = useState([]);
+  const { searchTerm } = useParams();
 
   useEffect(() => {
-    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`)
     .then(data => setVideos(data.items))
     .catch(err => console.error("Feed fetch error: ", err));
-  }, [selectedCategory]);
+  }, [searchTerm]);
 
   return (
-    <Stack sx={{ flexDirection: { sx: "column", md: "row" }}}>
-      <Box sx={{ 
-        height: { sx: 'auto', md: '92vh' }, 
-        borderRight: "1px solid #3d3d3d",
-        px: { sx: 0, md: 2 }
-      }}>
-        <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
-        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: '#fff' }}>
-          Copyright 2023 Wando Media
-        </Typography>
-      </Box>
+    <Box p={2} sx={{overflowY: 'auto', height: '90vh', flex: 2}}>
+      <Typography variant='h4' fontWeight="bold" mb={2} sx={{ color: 'white' }}>
+        Search results for <span style={{ color: '#F31503' }}>{searchTerm}</span> videos
+      </Typography>
 
-      <Box p={2} sx={{overflowY: 'auto', height: '90vh', flex: 2}}>
-        <Typography variant='h4' fontWeight="bold" mb={2} sx={{ color: 'white' }}>
-          {selectedCategory} <span style={{ color: '#F31503' }}>videos</span>
-        </Typography>
-
-        <Videos videos={videos} />
-      </Box>
-    </Stack>
+      <Videos videos={videos} />
+    </Box>
   )
 }
 
-export default Feed;
+export default SearchFeed;
