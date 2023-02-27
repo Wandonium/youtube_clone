b3f990401da8e6783672467da4f7f89ba9919514
Wandonium <hillarywando@gmail.com>
2023-02-27 12:50:40 +0300

    Fixed the category selection in the Sidebar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,16 @@
 import { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
 import { Sidebar, Videos } from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const Feed = () => {
+
+  const [selectedCategory, setSelectedCategory] = useState('New');
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`);
+  }, [selectedCategory]);
+
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" }}}>
       <Box sx={{ 
@@ -10,7 +18,7 @@ const Feed = () => {
         borderRight: "1px solid #3d3d3d",
         px: { sx: 0, md: 2 }
       }}>
-        <Sidebar />
+        <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: '#fff' }}>
           Copyright 2023 Wando Media
         </Typography>
