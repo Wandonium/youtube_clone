2c7eef099367e5fc181b9232f61eaa8ea40f578d
Wandonium <hillarywando@gmail.com>
2023-02-28 11:05:11 +0300

    Done updating the VideoDetails component. Done building the app. We now look into deploying to netlify.

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -4,26 +4,62 @@ import ReactPlayer from 'react-player';
 import { Typography, Box, Stack } from '@mui/material';
 import { CheckCircle } from '@mui/icons-material';
 
-import { Video } from './';
+import { Videos } from './';
 import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 function VideoDetail() {
   const [videoDetail, setVideoDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
   const { id } = useParams();
 
   useEffect(() => {
     fetchFromAPI(`videos?part=snippet,statistics&id=${id}`)
-    .then(data => setVideoDetail(data.item))
+    .then(data => setVideoDetail(data.items[0]))
     .catch(err => console.error("Fetch video details error: ", err));
+    
+    fetchFromAPI(`search?part=snippet&relatedToVideoId=${id}&type=video`)
+    .then(data => setVideos(data.items))
+    .catch(err => console.error("Fetch related videos error: ", err));
   }, [id]);
   
+  if(!videoDetail?.snippet) return "Loading...";
+
+  const { snippet: { title, channelId, channelTitle }, statistics: {
+    viewCount, likeCount } } = videoDetail;
+
   return (
     <Box minHeight="95vh">
       <Stack direction={{ xs: 'column', md: 'row'}}>
         <Box flex={1}>
           <Box sx={{ width: '100%', position: 'sticky', top: '86px' }}>
-            <ReactPlayer />
+            <ReactPlayer 
+              url={`https://www.youtube.com/watch?v=${id}`} 
+              className="react-player"
+              controls
+            />
           </Box>
+          <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
+            {title}
+          </Typography>
+          <Stack direction="row" justifyContent="space-between" sx={{ color: "#fff" }} px={2} py={1}>
+            <Link to={`/channel/${channelId}`}>
+              <Typography variant={{ sm: "subtitle1", md: "h6" }} color="#fff">
+                {channelTitle}
+                <CheckCircle sx={{ fontSize: '12px', color: 'gray', ml: '5px' }} />
+              </Typography>
+            </Link>
+            <Stack direction="row" gap="20px" alignItems="center">
+              <Typography variant="body1" sx={{ opacity: 0.7 }}>
+                {parseInt(viewCount).toLocaleString()} views
+              </Typography>
+              <Typography variant="body1" sx={{ opacity: 0.7 }}>
+                {parseInt(likeCount).toLocaleString()} likes
+              </Typography>
+            </Stack>
+          </Stack>
+        </Box>
+        <Box px={2} py={{ md: 1, xs: 5 }} justifyContent="center" alignItems="center" >
+          <Videos videos={videos} direction="column" />
         </Box>
       </Stack>
     </Box>
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,9 +1,9 @@
 import { Stack, Box } from '@mui/material';
 import { VideoCard, ChannelCard } from './';
 
-const Videos = ({ videos }) => {
+const Videos = ({ videos, direction }) => {
   return (
-    <Stack direction='row' flexWrap='wrap' justifyContent='start' gap={2}>
+    <Stack direction={ direction || 'row'} flexWrap='wrap' justifyContent='start' gap={2}>
         {videos.map((vid, idx) => (
             <Box key={idx}>
                 {vid.id.videoId && <VideoCard video={vid} />}
